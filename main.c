27c762383571bbb4db9d4f00ec3ea82430b18dd4
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2020-12-02 22:20:07 +0700

    commit

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+float funHipotenus(float A, float B);
 float funArea(float lebar, float tinggi);
-float funHipotenus(float a);
 
 int main(int argc, char** argv) {
 	float lebar, tinggi; 
@@ -13,23 +14,22 @@ int main(int argc, char** argv) {
 	printf("Tinggi : "); scanf("%f", &tinggi);
 	
     area = funArea(lebar, tinggi); 
-    hipotenus = funHipotenus(45);
+    hipotenus = funHipotenus(lebar, tinggi);
     
-    printf("Hipotenus %0.2f\n", hipotenus);
+    printf("Hipotenus = %0.2f\n", hipotenus);
     
     printf("Area = (1/2)*Lebar*Tinggi = %0.2f", area);
 	return 0;
+}
+
+float funHipotenus(float A, float B){
+	float value;
+	value = sqrt(A*A+B*B);
+	return value;
 }
 
 float funArea(float lebar, float tinggi){
 	float hasil;
 	hasil = (float) lebar*tinggi*0.5; //0.5 is 1/2
 	return hasil;
-}
-
-float funHipotenus(float a){
-	float value =a;
-	//put your math logic here
-	
-	return value;
 }
